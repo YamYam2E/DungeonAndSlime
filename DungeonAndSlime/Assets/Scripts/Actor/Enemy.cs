0bae38094730fcc35a74847d67aa8f4158c42139
MoonStarStudio <yamyam2e@gmail.com>
2023-02-28 01:10:56 +0900

    Add hp sprite and script
    to use enemies hp slider

--- DungeonAndSlime/Assets/Scripts/Actor/Enemy.cs
+++ DungeonAndSlime/Assets/Scripts/Actor/Enemy.cs
@@ -1,7 +1,21 @@
-﻿namespace Actor
+﻿using System;
+using UnityEngine;
+
+namespace Actor
 {
     public class Enemy : ActorBase
     {
-        
+        [SerializeField] private SpriteRenderer modelSpriteRenderer;
+        [SerializeField] private Texture2D texture;
+        //
+        public void Awake()
+        {
+            var sprites = Resources.LoadAll<Sprite>("Enemies/" + texture.name);
+            
+            foreach (var sprite in sprites)
+            {
+                Debug.Log(sprite.name);
+            }
+        }
     }
 }
--- DungeonAndSlime/Assets/Scripts/Ui/Game/Actor/HpProgress.cs
+++ DungeonAndSlime/Assets/Scripts/Ui/Game/Actor/HpProgress.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Ui.Game.Actor
 {
     public class HpProgress : MonoBehaviour
     {
+        [SerializeField] private SpriteRenderer sliderTarget;
+
+        private Vector2 defaultValue;
         
+        private void Awake()
+        {
+            defaultValue = sliderTarget.size;
+        }
+
+        public void SetValue(float rate)
+        {
+            var size = sliderTarget.size;
+            size = new Vector2(size.x * rate, size.y);
+            sliderTarget.size = size;
+        }
     }
 }
