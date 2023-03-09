bf551d86e739456a3f5be9b69e8e0c362b53bdde
MoonStarStudio <yamyam2e@gmail.com>
2023-03-09 19:08:41 +0900

    Set test enviroument for Addressable asset
    어드레서블 에셋을 사용하기 위해, 기능 확인과 함께 코드를 작성하는 중
    
    앞으로 다음과 같은 파일들이 AddressableAsset branch에 추가 될 것으로 예상
    - ResourceManager 관련 코드
    - AddressableAsset 관련 세팅 파일

---
+++ DungeonAndSlime/Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using Common;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Manager
+{
+    public class ResourceManager : MonoBehaviourSingleton<ResourceManager>
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Addressables.InstantiateAsync("Enemies/Enemy (1)").Completed += instantiate_Completed;
+        }
+
+        private void instantiate_Completed(AsyncOperationHandle<GameObject> obj) {
+            // Add component to release asset in GameObject OnDestroy event
+            //obj.Result.AddComponent(typeof(SelfCleanup));
+            Debug.Log(obj.Result);
+        }
+        
+        private void LoadAsset()
+        {
+        }
+    }
+}
