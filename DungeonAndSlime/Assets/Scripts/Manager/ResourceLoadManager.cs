473ba12d03705062bce113d80ea6d41f4940facd
MoonStarStudio <yamyam2e@gmail.com>
2023-03-13 18:39:12 +0900

    Update addressable loading class
    - To Prevent to duplicate class name (even they use other namespace), change 'ResourceManager' class to 'ResourceLoadManager'
    - Update addressable asset setting
      a. Add 'Remote' profile
      b. Add local path address in 'Addressables Profiles'
        (Window>Asset Management>Addressable>Profiles)
      c. Change some value in 'AddressableAssetSettings' scriptableObject
        1. Diagnostics>Send Profiler Events : true
        2. Diagnostics>Log Runtime Exceptions : true
        3. Content Update>Build Remote Catalog : true
        4. Build & Load Paths : Remote ( my costom profile )
    
    - If you need to build Addressable Asset, Make sure setting like them. and change 'Play Mode Script' to 'Use Excisting Build' in Addressables Groups Panel.
    
    - Before use addressable asset system. MUST understand what is this.

---
+++ DungeonAndSlime/Assets/Scripts/Manager/ResourceLoadManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Common;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Manager
+{
+    public enum EAddressType
+    {
+        EnemyAnimator,
+        EnemyAnimation,
+        EnemyTexture,
+    }
+    
+    public class ResourceLoadManager : MonoBehaviourSingleton<ResourceLoadManager>
+    {
+        public readonly Dictionary<string, Texture2D> preloadedTexture2Ds = new Dictionary<string, Texture2D>();
+      
+        public readonly Dictionary<string, AnimationClip> preloadedAnimations = new Dictionary<string, AnimationClip>();
+      
+        public RuntimeAnimatorController preloadedAnimator { get; private set; }
+
+        public bool IsDonePreload;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            //Test code for clean caching
+            Caching.ClearCache();
+            
+            StartCoroutine( LoadAnimatorAsset() );
+            // StartCoroutine( LoadAnimationAsset() );
+            // StartCoroutine( LoadTextureAsset() );
+        }
+
+        private IEnumerator LoadAnimatorAsset()
+        {
+            GameDebug.Log("-> Start to load Animator Asset by addressable");
+
+            var animatorResult
+                = Addressables.LoadAssetAsync<RuntimeAnimatorController>(EAddressType.EnemyAnimator.ToString());
+
+            while (!animatorResult.IsDone)
+                yield return null;
+
+            preloadedAnimator = animatorResult.Result;
+            
+            GameDebug.Log($"Completed to load Animator Asset by addressable");
+            
+            IsDonePreload = true;
+        }
+
+        private IEnumerator LoadAnimationAsset()
+        {
+            GameDebug.Log("-> Start to load Animation Asset by addressable");
+
+            var result = Addressables.LoadAssetsAsync<AnimationClip>(
+                EAddressType.EnemyAnimation.ToString(), null);
+
+            while (!result.IsDone)
+                yield return null;
+
+            foreach (var data in result.Result)
+                preloadedAnimations.Add(data.name, data);
+            
+            GameDebug.Log($"Completed to load Animation Asset by addressable : total [{preloadedAnimations.Count}]");
+        }
+        
+        private IEnumerator LoadTextureAsset()
+        {
+            GameDebug.Log("-> Start to load Texture2D Asset by addressable");
+            
+            var result = Addressables.LoadAssetsAsync<Texture2D>(
+                EAddressType.EnemyTexture.ToString(), null);
+
+            while (!result.IsDone)
+                yield return null;
+
+            foreach (var data in result.Result)
+            {
+                preloadedTexture2Ds.Add(data.name, data);
+                
+            }
+            
+            GameDebug.Log($"Completed to load Texture2D Asset by addressable : total [{preloadedTexture2Ds.Count}]");
+            
+            
+        }
+    }
+}
